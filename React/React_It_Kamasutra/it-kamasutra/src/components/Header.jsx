a08b8df88a746062a1e5570653d8d962a665afe2
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-11-07 21:34:18 +0200

    Finished lesson 12 IT-Kamasutra React

---
+++ React/React_It_Kamasutra/it-kamasutra/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.designevo.com/res/templates/thumb_small/blue-and-yellow-mansion.png' /> 
+        </header >
+    );
+}
+
+export default Header;
---
+++ React/React_It_Kamasutra/it-kamasutra/src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+    <div className='content'>
+      <div>
+        <img src="https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg" />
+      </div>
+      <div>
+        <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" />
+      </div>
+      <div>
+        My posts
+        <div> New post</div>
+      </div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+    );
+}
+
+export default Profile;
