5dcfe9720920ac8cbfcf1466a77cc05ace07e1b0
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-11-11 13:09:13 +0200

    Finished lesson 31 IT-kamasutra

--- React/React_It_Kamasutra/it-kamasutra/src/components/Dialogs/Message/Message.jsx
+++ React/React_It_Kamasutra/it-kamasutra/src/components/Dialogs/Message/Message.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import s from './../Dialogs.module.css';
 
 
+
 const Message = (props) => {
+    let messageRef = React.createRef();
+    const addMessage = () => {
+        let currentMes = messageRef.current.value;
+        alert(currentMes);
+    }
+
     return (
         <div className={s.message}>
             {props.message}
+            <p><textarea ref={messageRef}> </textarea></p>
+            <p><button onClick={addMessage}> Answer </button></p>
         </div>
         );
 }
--- React/React_It_Kamasutra/it-kamasutra/src/components/Profile/MyPost/MyPosts.jsx
+++ React/React_It_Kamasutra/it-kamasutra/src/components/Profile/MyPost/MyPosts.jsx
@@ -5,15 +5,21 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 
     let postElements = props.posts.map(p => <Post message={p.post} like={p.likesCount} />)
+    
+    let newPostElement = React.createRef();
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert (text);
+    };
 
     return (
     <div className={s.postsBlock}>
         <h3> My posts </h3>
         <div> 
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
-          <button>Add post</button>
+          <button onClick={ addPost }>Add post</button>
           <button>Remove post</button>
         </div>
         <div className={s.posts}>
