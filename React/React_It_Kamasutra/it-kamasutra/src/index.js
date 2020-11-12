2bd0991a461461fabd5c6edf7069e7f1bc9ccb74
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-11-13 00:33:09 +0200

    Finished lesson 37

--- React/React_It_Kamasutra/it-kamasutra/src/index.js
+++ React/React_It_Kamasutra/it-kamasutra/src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {addMessage, addPost, updateNewPostText, updateNewMessageText, subscribe} from './redux/state'
+import store from './redux/state'
 
 let rerenderEntiretree = (state) => {
     ReactDOM.render(
     <React.StrictMode>
-      <App state={state}
-       addPost={addPost}
-       updateNewPostText={updateNewPostText}
-       addMessage={addMessage}
-       updateNewMessageText={updateNewMessageText}/>
+      <App state={store.getState()}
+       addPost={store.addPost.bind(store)}
+       updateNewPostText={store.updateNewPostText.bind(store)}
+       addMessage={store.addMessage.bind(store)}
+       updateNewMessageText={store.updateNewMessageText.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntiretree(state);
+rerenderEntiretree(store.getState());
 
-subscribe(rerenderEntiretree);
+store.subscribe(rerenderEntiretree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- React/React_It_Kamasutra/it-kamasutra/src/redux/state.js
+++ React/React_It_Kamasutra/it-kamasutra/src/redux/state.js
@@ -1,10 +1,10 @@
-
+/* 
 let rerenderEntiretree = () => {
     console.log('State was changed');
 }
 
 
-let state = {
+let _state = {
     profilePage: {
         postData: [
         {id: 1, post: 'Hi, how are you?', likesCount: 12},
@@ -32,46 +32,117 @@ let state = {
     sidebar: {}
 }
 
-window.state = state;
+window._state = _state;
 
 export const addPost = () => {
 
     let newPost = {
         id: 6,
-        post: state.profilePage.newPostText,
+        post: _state.profilePage.newPostText,
         likesCount: 0
     };
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntiretree(state);
+    _state.profilePage.postData.push(newPost);
+    _state.profilePage.newPostText = '';
+    rerenderEntiretree(_state);
 }
 
 export const addMessage = () => {
     let newMessage = {
         id: 6,
-        message: state.dialogsPage.newMessageData
+        message: _state.dialogsPage.newMessageData
     }
-    state.dialogsPage.messagesData.push(newMessage);
-    state.dialogsPage.newMessageData = '';
-    rerenderEntiretree(state);
+    _state.dialogsPage.messagesData.push(newMessage);
+    _state.dialogsPage.newMessageData = '';
+    rerenderEntiretree(_state);
 }
 
 export const updateNewMessageText = (newMessage) => {
-    state.dialogsPage.newMessageData = newMessage;
-    rerenderEntiretree(state);
+    _state.dialogsPage.newMessageData = newMessage;
+    rerenderEntiretree(_state);
 }
 
 export const updateNewPostText = (newText) => {
 
-    state.profilePage.newPostText = newText;
-    rerenderEntiretree(state);
+    _state.profilePage.newPostText = newText;
+    rerenderEntiretree(_state);
 }
 
 export const subscribe = (observer) => {
     rerenderEntiretree = observer;
 }
 
-
-export default state;
+ */
+// export default _state;
 
 // store - OOP 
+
+
+let store = {
+
+    _state: {profilePage: {
+        postData: [
+        {id: 1, post: 'Hi, how are you?', likesCount: 12},
+        {id: 2, post: 'It\'s my first post', likesCount: 15},
+        {id: 3, post: 'Hi', likesCount: 25},
+        {id: 4, post: 'Hi', likesCount: 19},
+        {id: 5, post: 'Hi', likesCount: 9}],
+        newPostText: 'it-kamasutra.com'
+    },
+    dialogsPage : {
+        messagesData: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it-kamasutra?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'}],
+        dialogsData: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Lesha'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Zakhar'},
+            {id: 5, name: 'Vova'}],
+        newMessageData: 'write your message'
+    },
+    sidebar: {}
+},
+    getState () {
+        return this._state;
+    },
+    _callSubscriber () {
+        console.log('State was changed');
+    },
+    addPost () {
+        debugger;
+        let newPost = {
+            id: 6,
+            post: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    addMessage () {
+        let newMessage = {
+            id: 6,
+            message: this._state.dialogsPage.newMessageData
+        }
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.newMessageData = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newMessage) {
+        this._state.dialogsPage.newMessageData = newMessage;
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    }
+}
+export default store;
+window.store = store;
+
