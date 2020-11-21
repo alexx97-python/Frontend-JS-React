e45b2c2b2b202e7d2519e4adc5a995b65916f63c
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-11-21 19:02:30 +0200

    Changed navbar component

--- React_EnglishVocabulary/english_vocabulary/src/components/Navbar/Navbar.jsx
+++ React_EnglishVocabulary/english_vocabulary/src/components/Navbar/Navbar.jsx
@@ -3,26 +3,19 @@ import './Navbar.css';
 
 const Navbar = (props) => {
     return (
-    <div className="navbar">
-      <div className="dropdown">
-        <button className="dropbtn"> Train
-          <i className="fa fa-caret-down"></i>
-        </button>
-        <div className="dropdown-content">
-          <a href="#"> My tasks </a>
-          <a href="#"> Vocabulary </a>
-          <a href="#"> Grammar </a>
-          <a href="#"> Practise video </a>
-          <a href="#"> Speaking </a>
-        </div>
-      </div>
       <div>
-        <a href="#"> Online course </a>
-        <a href="#"> Speaking club </a>
-        <a href="#"> Online vebinars </a>
-        <a href="#"> Blog </a>
+        <header>
+          <div class="flex-element">
+              <img src="./img/logo-englishdom.png" alt="logo" />
+          </div>
+          <div class="flex-element">
+              <a href="#"> 0 800 33 00 66 </a> 
+              <a href="#"> <i class="fas fa-headset"></i>Контакты </a> 
+              <a href="#"> RU <i class="fas fa-angle-up"></i> </a>
+              <a href="#"> <i class="fas fa-sign-in-alt"></i> Войти </a>
+          </div>
+        </header>
       </div>
-    </div>
     );
 }
 
