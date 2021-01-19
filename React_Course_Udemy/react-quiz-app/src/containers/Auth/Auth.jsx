a0a6a2342e086307766f9a5588ccc662c2ae8078
Oleksii Sheiko <programmer.alex97@gmail.com>
2021-01-19 14:03:53 +0200

    Created Authorization component and styled it

--- React_Course_Udemy/react-quiz-app/src/containers/Auth/Auth.jsx
+++ React_Course_Udemy/react-quiz-app/src/containers/Auth/Auth.jsx
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.module.css'
 
 export default class Auth extends Component {
 
+    loginHandler = () => {
+
+    }
+
+    signupHandler = () => {
+
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+    }
+
     render(){
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+
+                    <form onSubmit= {this.submitHandler} className={classes.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <Button
+                          type="success"
+                          onClick={this.loginHandler}
+                        >Log In</Button>
+
+                        <Button
+                          type="primary"
+                          onClick={this.signupHandler}
+                        >Sign Up</Button>
+                    </form>
+                </div>
             </div>
         )
     }
