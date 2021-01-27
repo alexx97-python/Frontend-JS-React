f09f24c53fb31c7f00bb5243d03e19de1fa365c7
Oleksii Sheiko <programmer.alex97@gmail.com>
2021-01-27 22:28:59 +0200

    Refactored Auth component to aplly Redux

--- React_Course_Udemy/react-quiz-app/src/containers/Auth/Auth.jsx
+++ React_Course_Udemy/react-quiz-app/src/containers/Auth/Auth.jsx
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.css';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { auth } from '../../store/actions/auth';
 
 
 function validateEmail(email) {
@@ -10,7 +11,7 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-export default class Auth extends Component {
+class Auth extends Component {
     state = {
         isFormValid: '',
         formControls: {
@@ -41,35 +42,20 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try {
-            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBGXdFt0lvls9cP6pD-rfUrdt9rSXb8vw0', authData);
-        
-            console.log(response.data)
-        } catch (e){
-            console.log(e)
-        }
+    loginHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            true
+        )
     }
 
-    signupHandler = async () => {
-        const authData = {
-            email: this.state.formControls.email.value,
-            password: this.state.formControls.password.value,
-            returnSecureToken: true
-        }
-        try {
-            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBGXdFt0lvls9cP6pD-rfUrdt9rSXb8vw0`, authData);
-        
-            console.log(response.data)
-        } catch (e){
-            console.log(e)
-        }
-        
+    signupHandler = () => {
+        this.props.auth(
+            this.state.formControls.email.value,
+            this.state.formControls.password.value,
+            false
+        )
     }
 
     submitHandler = event => {
@@ -169,3 +155,11 @@ export default class Auth extends Component {
         )
     }
 }
+
+function mapDispatchToProps (dispatch) {
+    return{
+        auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+    }
+}
+
+export default connect (null, mapDispatchToProps) (Auth);
